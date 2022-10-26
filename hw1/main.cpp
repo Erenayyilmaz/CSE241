92c4edcccccff4cd9087ee3b99f7318f42b05678
Erenayyilmaz <kenaneren67@gmail.com>
2022-10-26 22:26:50 +0300

    26.10 shift and other collision state checkes started but small mistake on main havent seen yet

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,16 +1,61 @@
 #include "utils.h"
-
 using namespace std;
 
 int main()
 {
+	vector< vector <char> > merged_map
+	{
+		{'0','0','0','0'},
+		{'0','0','0','0'},
+		{'0','0','0','0'},
+		{'0','0','0','0'}
+	};
+	vector< vector <char> > merged_temp_map
+	{
+		{'0','0','0','0'},
+		{'0','0','0','0'},
+		{'0','0','0','0'},
+		{'0','0','0','0'}
+	};
 	vector <Tetromino> arr;
+
 	//Tetromino a;
-	//  e_typ k;
-	//  k = e_typ::j;
-	//  Tetromino	a(k);
+	  //e_typ k;
+	  //k = e_typ::j;
+	  //Tetromino	a(k);
 	//arr.push_back(a);
 	get_info(arr);
+	cout << "bura geld aaaaa" << endl ;
+	//merged_map = a.get_coll_map();
+	merged_map = arr[0].get_coll_map();
+	cout << "bura geld bb" << endl ;
+	merged_temp_map = merge_coll_maps(arr[0],merged_map);//arr[0] ile erişemiyorum..
+//	merged_temp_map = merge_coll_maps(a,merged_map);
+	if (!merged_temp_map.size())
+		cout<<"zize: 0";
+	cout << "bura geld ddddd" << endl ;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "for 1ici girdi"<<endl;
+		for (int k = 0; k< 8; i++)
+		{
+			cout << "for ici girdi"<<endl;
+			cout << merged_temp_map[i][k];
+			cout << "merge map sonu"<<endl;
+		}
+		cout << endl;
+	}
+	
+	for (int i = 1; i < arr.size(); i++)
+	{
+		//int	best_surface_coll_counter = most_surf(merged_map);
+		for (int j = 0; j < 4; j++)//4 rotatoion
+		{
+			//shift
+		}
+		
+	}
+	
 	return (0);
 }
 
